a0f5f47d31961a8a5fe8e0e8ad5e1de3f6aca1bf
LShuyanLi <sli341@my.bcit.ca>
2025-09-22 11:56:31 -0700

    Add files via upload

---
+++ primitiveTypes.js
@@ -0,0 +1,23 @@
+console.log('===============STRINGS=============');
+console.log(typeof 'Hello'+1);
+console.log('Hello'+1); //Hello1
+
+console.log('===============NUMBER=============');
+console.log(typeof 5);
+console.log(5+1); //6
+console.log('5'+1); //51
+console.log(5+'1'); //51
+console.log('5'+'1'); //51
+console.log(5+1+'1'); //61
+console.log(5+'1'+1); //511
+
+console.log('===============BOOLEANS=============');
+console.log(typeof true, typeof false);
+// Decision Marker
+console.log('Hi' === 'Bye'); //false
+console.log('Hi' === 'Hi'); //true
+console.log('' === 'password'); //false
+console.log('5' === 5); //false
+console.log(Number['5'] === 5); //false
+console.log('5' == 5); //ture
+
