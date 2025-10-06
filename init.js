6eb157a4232dc54d8e9f846b75bf925187520685
LShuyanLi <sli341@my.bcit.ca>
2025-10-06 12:26:38 -0700

    Add files via upload

--- init.js
+++ init.js
@@ -1,48 +1,66 @@
+const { log } = require('console');
 
-// Import the readline module and create an interface for reading input/output from the console
+// Import the readline module and create an interface to read input from the console
 const readline = require('readline').createInterface({
-  input: process.stdin,   // Take input from the standard input (keyboard)
-  output: process.stdout  // Output text to the console
+  input: process.stdin,  // Input will come from standard input (keyboard)
+  output: process.stdout // Output will go to standard output (console)
 });
 
-// Counter to track how many times the question has been asked
-let ind = 1;
 
-/**
- * Function that repeatedly asks the user for input
- */
-function AskName() {
-  readline.question('Hello! Do you want to hear story? (type "exit" to quit): ', input => {
-    
-    // If user types "exit", end the program
-    if (input.toLowerCase() === 'exit') {
-      console.log("Thanks, hope you like the storiy, BYE!");
-      readline.close(); // Close the input stream
-      return;
+// Function to ask the user for their name
+function AskStudentName() {
+  readline.question(
+    'What is your name? (type "exit" to exit): ',
+    input => {
+       uname = input
+      // If the user types "exit", end the program
+      if (input.toLowerCase() === 'exit') {
+        console.log("Goodbye!");
+        readline.close(); // Close the input stream
+        return;
+      }
+
+      if (!uname) {
+        console.log("Wrong name. Try Again!");
+        AskStudentName()
+      } else {
+        console.log("Wellcome " + uname + "!");
+        AskCourseName()
+        
+      }
+
+      // Function to ask the user for the name of a course they want to add
+
+    function AskCourseName(){
+  readline.question(
+    'Hi ' + uname + ', what is the name of the course you want to add? (type "exit" to exit): ',
+    input => {
+       cname = input
+      // If the user types "exit", end the program
+      if (input.toLowerCase() === 'exit') {
+        console.log("Goodbye!");
+        readline.close(); // Close the input stream
+        return;
+      }
+
+      if (uname) {
+        console.log("Wellcome " + uname + " from " + cname);
+        readline.close(); // Close the input stream
+        return;
+      } else {
+        console.log("Try again!");
+        AskStudentName()
+      }
+      // You can add further processing of the input here if needed
     }
-
-    // Respond differently based on the number of times the question has been asked
-    if (ind === 1) {
-      console.log("In the forest, there is a girl who live with her grandmother...", input);
-    } else if (ind === 2) {
-      console.log("But there is also a horrible wolf named Jack live in the same forest...", input);
-    } else if (ind === 3) {
-      console.log("The girl named red hat, she is strong enough to protect her grandmother and herself...", input);
-    } else if (ind === 4) {
-      console.log("So, Jack is too hunger that ask the girl for help, she gives Jack some fruits and vagetebales..", input);
-    } else if (ind === 5) {
-      console.log("Jack finds that so delicious there is. Finally, Jack amd Red Hat bacome the best friends in this forest.(end, type 'exit' to quit)");
+  );
+}
       
+
+      // You can add further processing of the input here if needed
     }
-
-    // Increment the counter for the next round
-    ind = ind + 1;
-
-    // Recursively call the function to keep the loop going
-    AskName();
-  });
+  );
 }
 
-// Start the input loop
-AskName();
-
+// Start the interaction by asking for the student's name
+AskStudentName();
